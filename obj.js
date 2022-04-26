0f88eeed133810954d72bc9bd3be1c18ce6f884a
zerius182 <garryjoynes182@hotmail.co.uk>
2022-04-26 14:22:43 +0100

    Added prototype to Book to show info in console

--- obj.js
+++ obj.js
@@ -16,6 +16,15 @@ Book.prototype.AddImgUrl = function(newUrl){
     this.imgUrl = newUrl
 }
 
+Book.prototype.ShowInfoInConsole = function(){
+    for (let key in this){
+        if (this.hasOwnProperty(key)){
+            console.log(`${key}: ${this[key]}`)
+        }
+    }
+}
+
 const demoBook = new Book("The Eye Of The World", "Robert Jordan", 800);
 demoBook.ReadBook();
 demoBook.AddImgUrl("https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg");
+demoBook.ShowInfoInConsole();
