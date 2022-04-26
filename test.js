998d95d05dea3b2a3338c17d32b10a3a1c2683f9
zerius182 <garryjoynes182@hotmail.co.uk>
2022-04-26 10:42:21 +0100

    Added new test file, added test stuff to test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+let testBook = new Book("test-title", "test-author", 666);
+testBook.ReadBook();
+testBook.AddImgUrl("www.google.com");
+bookLibrary.push(testBook);
+let test2 = new Book("yo", "yo", 999);
+bookLibrary.push(test2);
+
+bookLibrary.forEach(book =>{
+    for (let key in book){
+        if(book.hasOwnProperty(key)){
+            console.log(`${key}: ${book[key]}`);
+        }
+        else{
+            continue;
+        }
+    }
+})
