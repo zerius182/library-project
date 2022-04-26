dca3ce6531a81cc742115209dd89ccc6a409ef0d
zerius182 <garryjoynes182@hotmail.co.uk>
2022-04-26 10:47:36 +0100

    Added demo book

--- obj.js
+++ obj.js
@@ -15,3 +15,7 @@ Book.prototype.ReadBook = function(){
 Book.prototype.AddImgUrl = function(newUrl){
     this.imgUrl = newUrl
 }
+
+const demoBook = new Book("The Eye Of The World", "Robert Jordan", 800);
+demoBook.ReadBook();
+demoBook.AddImgUrl("https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg");
