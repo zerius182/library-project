573af0e36a73a66a692d2bff9f5cc906d3b00f25
zerius182 <garryjoynes182@hotmail.co.uk>
2022-04-29 11:22:53 +0100

    Refined addBook script to take an optional imgUrl

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const bookGrid = document.querySelector("#book-grid");
 const bookLibrary = [];
 
-function Book(title, author, pages){
+function Book(title, author, pages, imgUrl){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = false;
-    this.imgUrl = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
+    this.imgUrl = imgUrl;
+    if(this.imgUrl === undefined){
+        this.imgUrl = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
+    }
 }
 
 Book.prototype.ReadBook = function(){
@@ -25,8 +28,8 @@ Book.prototype.ShowInfoInConsole = function(){
     }
 }
 
-function addNewBook(newTitle, newAuthor, newPages){
-    let newBook = new Book(newTitle, newAuthor, newPages);
+function addNewBook(newTitle, newAuthor, newPages, newImgUrl){
+    let newBook = new Book(newTitle, newAuthor, newPages, newImgUrl);
     bookLibrary.push(newBook);
 }
 
@@ -78,10 +81,9 @@ function populateBookGrid(libraryArray){
 
 
 
-addNewBook("Wheel of Time", "Robert Jordan", 800);
+addNewBook("The Eye of the World", "Robert Jordan", 800, "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg");
 addNewBook("Bible", "God", 7777);
-addNewBook("Necronomicom", "Satan", 666);
-bookLibrary[0].AddImageUrl("https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg");
+addNewBook("Necronomicom", "Satan", 666, "https://www.sideshow.com/storage/product-images/905430/book-of-the-dead-necronomicon_evil-dead-ii_silo.png");
 
 
 populateBookGrid(bookLibrary);
